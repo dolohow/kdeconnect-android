96825721863008f4602ca151bf754605064c10f5
Łukasz Żarnowiecki <dolohow@outlook.com>
2016-09-18 23:54:09 +0200

    Implement flashlight flashing in FinMyPhone plugin

--- src/org/kde/kdeconnect/Plugins/FindMyPhonePlugin/FindMyPhoneActivity.java
+++ src/org/kde/kdeconnect/Plugins/FindMyPhonePlugin/FindMyPhoneActivity.java
@@ -2,20 +2,62 @@ package org.kde.kdeconnect.Plugins.FindMyPhonePlugin;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.hardware.Camera;
 import android.media.AudioAttributes;
 import android.media.AudioManager;
 import android.media.Ringtone;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
 import org.kde.kdeconnect_tp.R;
 
+class Flashlight implements Runnable {
+    public boolean stopRunning = false;
+
+    @Override
+    public void run() {
+        Camera camera;
+        try {
+            camera = Camera.open();
+        } catch (RuntimeException re) {
+            Log.d("FindMyPhone", "Device does not have a camera");
+            return;
+        }
+
+        Camera.Parameters flashlightOn = camera.getParameters();
+        Camera.Parameters flashlightOff = camera.getParameters();
+
+        flashlightOn.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
+        flashlightOff.setFlashMode(Camera.Parameters.FLASH_MODE_OFF);
+
+        while (!stopRunning) {
+            try {
+                camera.setParameters(flashlightOn);
+                Thread.sleep(40);
+                camera.setParameters(flashlightOff);
+                Thread.sleep(40);
+            } catch (InterruptedException ignored) {
+
+            } catch (RuntimeException re) {
+                Log.d("FindMyPhone", "Device does not have a flashlight");
+                camera.release();
+                return;
+            }
+
+        }
+
+        camera.release();
+    }
+}
+
 public class FindMyPhoneActivity extends Activity {
     Ringtone ringtone;
+    Flashlight flashlight;
 
     @Override
     protected void onNewIntent(Intent intent) {
@@ -32,6 +74,10 @@ public class FindMyPhoneActivity extends Activity {
         window.addFlags(WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD |
                         WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED |
                         WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON);
+
+        flashlight = new Flashlight();
+        Thread flashlightThread = new Thread(flashlight);
+        flashlightThread.start();
 
         Uri ringtoneUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_RINGTONE);
         ringtone = RingtoneManager.getRingtone(getApplicationContext(), ringtoneUri);
@@ -57,6 +103,7 @@ public class FindMyPhoneActivity extends Activity {
     @Override
     public void finish() {
         ringtone.stop();
+        flashlight.stopRunning = true;
         super.finish();
     }
 }
